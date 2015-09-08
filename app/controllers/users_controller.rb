fa4eeac5321dae47a2b1af8e7e07a0bc9ff0c7ba
James Bomotti <zilliahix@gmail.com>
2015-09-08 11:06:09 -0700

    Add User creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
+
   def new
   end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = 
+  end
+
+
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :username, :email, :password)
+    end
+
 end
