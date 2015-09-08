b6ecb320cc81197ae2da6af1016be2ace42ce84d
James Bomotti <zilliahix@gmail.com>
2015-09-08 15:28:48 -0700

    Add Log Out, Materialize CSS, link to Register

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,23 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(username: params[:username])
-
+    user = User.find_by(:username => params[:session][:username])
     if (user && user.password == params[:password])
       auth_login(user)
-      redirect_to '/'
+      render 'index'
     end
   end
 
   def destroy
+    p "Session before: #{session[:user_id]}"
+    session.destroy
+    p "Session after: #{session[:user_id]}"
+    render 'index'
   end
 
   private
     def auth_login(user)
       session[:user_id] = user.id
     end
+    
 end
