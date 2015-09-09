2bd283feafb0732af30e2bee8f2e2c3406263b42
James Bomotti <zilliahix@gmail.com>
2015-09-08 17:26:06 -0700

    Add Languages (MVC)

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,20 @@
 module SessionsHelper
+
+  def auth_logged_in?
+    !session[:user_id].nil?
+  end
+
+  def auth_login(user)
+    session[:user_id] = user.id
+  end
+
+  def auth_logout
+    session.destroy
+  end
+
+  def auth_current_user
+    return nil unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150908173328) do
+ActiveRecord::Schema.define(version: 20150908225127) do
+
+  create_table "languages", force: :cascade do |t|
+    t.string   "name"
+    t.string   "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
