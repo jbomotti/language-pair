cba724ad56cf3f3b49ed9c53cc2ba0cb66cd15d4
James Bomotti <zilliahix@gmail.com>
2015-09-08 17:45:18 -0700

    Add new Language button, form, and routes

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -1,9 +1,30 @@
 class LanguagesController < ApplicationController
 
   def index
+    @languages = Language.all
   end
 
   def new
+    @language = Language.new
   end
-  
+
+  def create
+    @language = Language.new(language_params)
+
+    if @language.save
+      redirect_to languages_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @languages = Language.all
+  end
+
+  private
+  def language_params
+    params.require(:language).permit(:name)
+  end
+
 end
